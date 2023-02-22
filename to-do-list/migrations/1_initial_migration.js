12e16292644ca3a26af8d13987414960069c5dd3
Nitant <96972634+nitantchhajed@users.noreply.github.com>
2023-02-22 12:28:14 +0530

    Delete to-do-list directory

--- to-do-list/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const Todolist = artifacts.require("Todolist");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Todolist);
-};
